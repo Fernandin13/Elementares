9281cda34f15268a23113806842de6524aa2c1bf
Fernandin13 <fernando.sferreyra@gmail.com>
2022-01-19 17:37:55 -0300

    .

--- Fogo.js
+++ Fogo.js
@@ -7,14 +7,11 @@ class Fogo{
         this.body = Bodies.rectangle(x,y,width,height,options)
         this.width = width
         this.height = height
-        this.image = loadImage("Elementals_fire_knight_basic_v1.0/gifs/01_idle.gif")
         World.add(world,this.body);
     }
     display(){
         var poss = this.body.position
-        push();
-        imageMode(CENTER)
-        image(this.image,poss.x,poss.y,this.width,this.height)
-        pop();
+        fogo = createSprite(200,200,10,10);
+        this.body = fogo.addAnimation("parado",this.animation)
     }
-}
+} 
--- sketch.js
+++ sketch.js
@@ -4,19 +4,27 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var fogo;
-var agua;
-var imagemfogo,imagemagua;
+var vento;
+var imagemfogo,imagemvento;
 var fundoimg;
+var ground;
+var animation1;
 
 function preload(){
 fundoimg = loadImage("game_background_4.png")
+animation1 = loadAnimation("Elementals_fire_knight_basic_v1.0/gifs/01_idle.gif")
+
 }
 function setup(){
 createCanvas(windowWidth,windowHeight)
 engine = Engine.create(); 
 world = engine.world;
+fogo = new Fogo(200,200)
+ground = new Ground(600,height,1200,20)
 }
 function draw(){
 background(fundoimg)
 Engine.update(engine);
+fogo.display();
+ground.display();
 }
